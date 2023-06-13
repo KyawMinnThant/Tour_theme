09e258d2d19dd81e4d5589c6be011813cea5677c
KyawMinnThant <113605240+KyawMinnThant@users.noreply.github.com>
2023-06-13 17:39:34 +0630

    Update Path.jsx

--- src/routes/Path.jsx
+++ src/routes/Path.jsx
@@ -1,23 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Blog from "../pages/Blog";
 import Contact from "../pages/Contact";
-import About from "../pages/About";
-import Service from "../pages/Service";
+import Searchmodal from "../components/Searchmodal";
 
 const Path = () => {
+  const [modal, setModal] = useState(false);
   return (
-    <div>
-      <Navbar />
+    <div className=" relative">
+      {modal === true && (
+        <Searchmodal modal={modal} setModal={setModal} className=" " />
+      )}
+      <Navbar modal={modal} setModal={setModal} />
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About/>} />
-        <Route path='/service' element={<Service/>} />
       </Routes>
       <Footer />
     </div>
